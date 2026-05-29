8742deb5a8d963366e0abe483a2a2b699017723e
Yazan <yazazz6@gmail.com>
2026-05-30 02:49:06 +0300

    fix: remove 'en' from locale cast in i18n/request.ts - routing only supports ar

--- i18n/request.ts
+++ i18n/request.ts
@@ -4,7 +4,7 @@ import { routing } from './routing';
 export default getRequestConfig(async ({ requestLocale }) => {
   let locale = await requestLocale;
 
-  if (!locale || !routing.locales.includes(locale as 'ar' | 'en')) {
+  if (!locale || !routing.locales.includes(locale as 'ar')) {
     locale = routing.defaultLocale;
   }
 
